c7bd50d498f579ed4ca6f67aed1224bc5d63d958
chinatsuosawa <c.osawa@nt-21.co.jp>
2016-10-30 12:33:00 +0000

    task6 retake add

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,8 +15,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # storage :fog
 
   def public_id
-#    model.id
-    return micropost.id
+    model.id
+#    micropost.id
   end
 
   # Override the directory where uploaded files will be stored.
